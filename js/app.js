e2b5f347522c7c1042c3e70dbc36c61314c243d2
toooooby64 <jalvarez5667@gmail.com>
2023-12-24 11:40:57 -0800

    Added functionallity for family christmas party

--- js/app.js
+++ js/app.js
@@ -1,93 +1,90 @@
-let names = []
+let names = [];
 
-function addName(){
-    event.preventDefault();
-    // Grab/Create all HTML needed
-    const name = document.getElementById('name-input').value;
-    if (!validateInput(name)){
-        alert('Please enter a valid input')
-        return
-    }
-    if (name.includes(' ')){
-        names.push.apply(names, name.split(' '))
-        allNames = name.split(' ')
+function addName() {
+  event.preventDefault();
+  // Grab/Create all HTML needed
+  const name = document.getElementById("name-input").value.trimEnd();
 
-        allNames.forEach(name => {
-            createNameCards(name)
-        });
-    }else{
-        names.push(name);
-        createNameCards(name)
-    }
-    organizeNameCards();
-    
-    const nameInput = document.getElementById('name-input');
-    nameInput.value = '';
+  if (!validateInput(name)) {
+    alert("Please enter a valid input");
+    return;
+  }
+  console.log(name);
+  if (name.includes(",")) {
+    names.push.apply(names, name.split(","));
+    allNames = name.split(",");
 
-    window.sessionStorage.setItem("names", names)
-}
-
-function validateInput(name){
-    isEmpty = false
-    if (name.trim() === ''){
-        isEmpty = true
-    }
-    // Regular expression that matches any string containing characters other than alphanumeric characters and spaces
-    const regex = /^([a-zA-Z]+ +)*[a-zA-Z]+$/;
-    // Test the input string against the regular expression
-    return regex.test(name) && !isEmpty;
-}
-
-function createNameCards(name){
-    const container = document.getElementById('names-list');
-    const listItem = document.createElement('li');
-    const nameCard = document.createElement('div');
-    const button = document.createElement('button');
-
-
-    // Add name to nameCard
-    nameCard.innerText = name;
-    listItem.appendChild(nameCard)
-    //Add id and text to button
-    button.classList.add('delete-button');
-    button.innerText = 'X';
-
-    // Add name-card class to nameCard
-    nameCard.classList.add('name-card');
-
-    // Add event listener to button to remove name from array and DOM when clicked
-    button.addEventListener('click', function(){
-        const index = names.indexOf(name); // Find index of name
-        if (index > -1) {
-            names.splice(index, 1); // Remove name from array
-        }
-        window.sessionStorage.setItem("names", names)
-        listItem.remove(); // Remove li from DOM      
-          
+    allNames.forEach((name) => {
+      createNameCards(name);
     });
+  } else {
+    names.push(name);
+    createNameCards(name);
+  }
+  organizeNameCards();
 
-    // Add name and button to nameCard
-    nameCard.appendChild(button);
-    container.appendChild(listItem);
+  const nameInput = document.getElementById("name-input");
+  nameInput.value = "";
+
+  window.sessionStorage.setItem("names", names);
+}
+
+function validateInput(name) {
+  isEmpty = false;
+  if (name.trim() === "") {
+    isEmpty = true;
+  }
+  // Regular expression that matches any string containing characters other than alphanumeric characters, spaces, and commas
+  const regex = /^([a-zA-Z]+(, +)?)*[a-zA-Z]+$/;
+  // Test the input string against the regular expression
+  return regex.test(name) && !isEmpty;
+}
+
+function createNameCards(name) {
+  const container = document.getElementById("names-list");
+  const listItem = document.createElement("li");
+  const nameCard = document.createElement("div");
+  const button = document.createElement("button");
+
+  // Add name to nameCard
+  nameCard.innerText = name;
+  listItem.appendChild(nameCard);
+  //Add id and text to button
+  button.classList.add("delete-button");
+  button.innerText = "X";
+
+  // Add name-card class to nameCard
+  nameCard.classList.add("name-card");
+
+  // Add event listener to button to remove name from array and DOM when clicked
+  button.addEventListener("click", function () {
+    const index = names.indexOf(name); // Find index of name
+    if (index > -1) {
+      names.splice(index, 1); // Remove name from array
+    }
+    window.sessionStorage.setItem("names", names);
+    listItem.remove(); // Remove li from DOM
+  });
+
+  // Add name and button to nameCard
+  nameCard.appendChild(button);
+  container.appendChild(listItem);
 }
 
 function organizeNameCards() {
-    let nameCol = document.createElement('div');
-    nameCol.className = 'name-card-col';
+  let nameCol = document.createElement("div");
+  nameCol.className = "name-card-col";
 
-    let namesList = document.querySelector('#names-list');
-    let nameCards = namesList.querySelectorAll('li');
+  let namesList = document.querySelector("#names-list");
+  let nameCards = namesList.querySelectorAll("li");
 
-    nameCards.forEach((nameCard, index) => {
-        nameCol.appendChild(nameCard);
+  nameCards.forEach((nameCard, index) => {
+    nameCol.appendChild(nameCard);
 
-        if ((index + 1) % 5 === 0 || index === nameCards.length - 1) {
-            namesList.appendChild(nameCol);
-            nameCol = document.createElement('div');
-            nameCol.className = 'name-card-col';
-        }
-    });
+    if ((index + 1) % 5 === 0 || index === nameCards.length - 1) {
+      namesList.appendChild(nameCol);
+      nameCol = document.createElement("div");
+      nameCol.className = "name-card-col";
+    }
+  });
 }
-
-
-
--- js/groups.js
+++ js/groups.js
@@ -1,45 +1,62 @@
 //TODO - Add random name to group which has less ppl
 
-let groups = {}
+let groups = {};
 
-function createGroups(){
-    const groupNumber = document.getElementById('group-input').value;
-    const names = window.sessionStorage.getItem("names")
-    let namesArray = names.split(',')
-    // Shuffle namesArray
-    namesArray = namesArray.sort(() => Math.random() - 0.5);
-    
+function createGroups() {
+  const groupNumber = document.getElementById("group-input").value;
+  const names = window.sessionStorage.getItem("names");
+  let namesArray = names.split(",");
+  // Shuffle namesArray
+  namesArray = namesArray.sort(() => Math.random() - 0.5);
 
-    for (let i = 0; i < groupNumber; i++){
-        groups[i] = []
+  for (let i = 0; i < groupNumber; i++) {
+    groups[i] = [];
+  }
+
+  namesArray.forEach(function (name, index) {
+    let groupIndex = index % groupNumber; // Use modulo to cycle through groups
+    nameIndex = getRandomInt(namesArray.length);
+    groups[groupIndex].push(name);
+  });
+
+  for (let key in groups) {
+    maxGroupSize = groups[0].length;
+
+    if (groups[key].length < maxGroupSize) {
+      let randomName = namesArray[getRandomInt(namesArray.length)];
+      while (groups[key].includes(randomName)) {
+        randomName = namesArray[getRandomInt(namesArray.length)];
+      }
+      groups[key].push(randomName + "1");
     }
-
-    namesArray.forEach(function(name,index){
-        let groupIndex = index % groupNumber; // Use modulo to cycle through groups
-        nameIndex = getRandomInt(namesArray.length)
-        groups[groupIndex].push(name)
-    })
-    displayGroups()
+  }
+  displayGroups();
 }
 
-function displayGroups(){
-    const groupsList = document.getElementById('groups-list');
-    const lineBreak = document.createElement('br');
-    const div = document.createElement('div');
-    const groupRow = document.createElement('div');
-    groupRow.classList.add('group-row');
-    for (let key in groups){
-        const groupCards = document.createElement('div');
-        groupCards.innerHTML = `<div>Group ${parseInt(key) + 1}</div>`;
-        groupCards.classList.add('group-card');
-        groups[key].forEach(element => {
-            const nameCard = document.createElement('div');
-            nameCard.innerText = element;
-            groupCards.appendChild(nameCard);
-        });
-        groupRow.appendChild(groupCards);
-        groupsList.appendChild(groupRow);
-    }
+function displayGroups() {
+  const groupsList = document.getElementById("groups-list");
+  const lineBreak = document.createElement("br");
+  const div = document.createElement("div");
+  const groupRow = document.createElement("div");
+  groupRow.classList.add("group-row");
+  for (let key in groups) {
+    const groupCards = document.createElement("div");
+    groupCards.innerHTML = `<div>Group ${parseInt(key) + 1}</div>`;
+    groupCards.classList.add("group-card");
+    groups[key].forEach((element) => {
+      const nameCard = document.createElement("div");
+      if (element.includes("1")) {
+        element = element.slice(0, -1);
+        nameCard.innerText = element;
+        nameCard.style.fontWeight = "bold";
+      } else {
+        nameCard.innerText = element;
+      }
+      groupCards.appendChild(nameCard);
+    });
+    groupRow.appendChild(groupCards);
+    groupsList.appendChild(groupRow);
+  }
 }
 
 function getRandomInt(max) {
